1c63ece6788ee8ac7d57c6f6605a16a29d280957
Sam James <samjojames@gmail.com>
2022-07-18 14:17:25 +0100

    First commit

---
+++ src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Box from "@mui/material/Box";
+
+export function Navbar() {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box
+      display="flex"
+      justifyContent="flex-end"
+      alignItems="center"
+      paddingTop="2em"
+    >
+      <Box sx={{ maxWidth: { xs: 320, sm: 480 }, color: "#FFFFFF" }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          variant="scrollable"
+          scrollButtons={false}
+          aria-label="scrollable prevent tabs example"
+          indicatorColor="primary"
+        >
+          <Tab label="Home" sx={{ color: "#FFFFFF" }} />
+          <Tab label="About Me" sx={{ color: "#FFFFFF" }} />
+          <Tab label="Contact" sx={{ color: "#FFFFFF" }} />
+        </Tabs>
+      </Box>
+    </Box>
+  );
+}
+
+export default Navbar;
---
+++ src/Pages/Home/Home.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "../../App.css";
+import { Box, Button, Typography } from "@mui/material";
+import Navbar from "../../Components/Navbar";
+
+function Home() {
+  return (
+    <Box className="Home" height="100vh" paddingX="10em">
+      <Navbar />
+      <Box
+        marginTop="5em"
+        marginLeft="5em"
+        display="flex"
+        flexDirection="column"
+        gap="20px"
+        maxWidth={"50em"}
+      >
+        <Typography variant="h4" color="#FF31AD">
+          Hi, my name is
+        </Typography>
+        <Typography variant="h1" color="#ffffff">
+          Sam James
+        </Typography>
+        <Typography variant="h2" color="#B0B5BC">
+          Full Stack Developer. Desiging and implementing interactive web
+          experiences
+        </Typography>
+        <Button
+          variant="outlined"
+          href="mailto:samjojames@gmail.com"
+          target="_blank"
+          sx={{ maxWidth: "150px", color: "#FF31AD", borderColor: "#FF31AD" }}
+        >
+          Get in touch!
+        </Button>
+      </Box>
+      {/* <IconList /> */}
+    </Box>
+  );
+}
+
+export default Home;
